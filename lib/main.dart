6f727fc3d251797766ba9c22f365214db8345ab9
unain <fukangnow@gmail.com>
2018-09-17 17:11:29 +0800

    add debugPaintSizeEnabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:daily_helper/my_navigator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-void main() => runApp( MyApp());
+void main() => (){
+  debugPaintSizeEnabled=true;
+  runApp( MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
