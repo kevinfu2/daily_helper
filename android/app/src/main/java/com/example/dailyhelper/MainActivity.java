aaf89bd21e4dcc454ce7655f1a2661700d5b9ade
unain <fukangnow@gmail.com>
2018-09-18 10:27:17 +0800

    firebase config

--- android/app/src/main/java/com/example/dailyhelper/MainActivity.java
+++ android/app/src/main/java/com/example/dailyhelper/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.dailyhelper;
+package com.unain.dailyhelper;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,20 @@ import 'package:daily_helper/my_navigator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-void main() => (){
+void main(){
   debugPaintSizeEnabled=true;
   runApp( MyApp());
-};
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '一个测试',
+      title: '手机助手',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        brightness: Brightness.dark,
+        brightness: Brightness.light,
         primaryColor: Colors.lightBlue[800],
         accentColor: Colors.cyan[600],
         primarySwatch: Colors.blue,
